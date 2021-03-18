f638a86d95ad16443187c8499dfed29b2dab895b
pedrovitors <pedrovitorfhs@gmail.com>
2021-03-18 16:26:06 -0300

    Carrinho versao 1.20 - Agora o carrinho nÃao e mais tao feio

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -2,9 +2,16 @@ import React from "react"
 import styled from "styled-components"
 
 const CarrinhoContainer = styled.div`
+display:flex;
+flex-direction: column;
+width: 200px;
+margin: 10px 10px;
 border: 1px solid black;
+padding: 20px;
+
 `
 const BotaoRemover = styled.button`
+
 `
 
 export class Carrinho extends React.Component {
@@ -12,14 +19,12 @@ export class Carrinho extends React.Component {
     render() {
         return (
             <CarrinhoContainer>
-                <h3> Carrinho de Compras </h3>
-                <p> {this.props.nomeProduto} </p>
-                <p> {this.props.precoProduto} </p>
-                <p> {this.props.quantidadeProduto} </p>
+                <p> Item: {this.props.nomeProduto} </p>
+                <p> Preço: R$ {this.props.precoProduto} </p>
+                <p> Quantidade: {this.props.quantidadeProduto} </p>
                 <BotaoRemover onClick={() => this.props.onClickRemoverProduto()}>
                     Remover
                 </BotaoRemover>
-                <p> Valor total: R$ {this.props.valorTotalCompras}</p>
             </CarrinhoContainer>
         )
     }
